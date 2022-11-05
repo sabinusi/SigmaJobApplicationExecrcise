5434c5a2d65b4329c7b678c606d472286937e035
sabinusi mlambo <sabinusimlambo@gmail.com>
2022-11-05 19:59:23 +0300

    adding applicantMap class

---
+++ Model/ApplicantMap.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration;
+namespace SigmaJobApplicantion.Model;
+
+public class ApplicantMap:ClassMap<Applicant>
+{
+    public ApplicantMap()
+    {
+        Map(m => m.FirstName).Name("FirstName");
+        Map(m => m.LastName).Name("LastName");
+        Map(m => m.Email).Name("Email");
+        Map(m => m.CallTime).Name("CallTime");
+        Map(m => m.PhoneNumber).Name("PhoneNumber");
+        Map(m => m.LinkedInProfileLink).Name("LinkedAccount");
+        Map(m => m.GitHubProfileLink).Name("GitHubAccount");
+        Map(m => m.FreeText).Name("FreeText");
+    }
+}
